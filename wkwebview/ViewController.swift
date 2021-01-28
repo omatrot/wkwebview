fe4092ba9469402f9a3535164b1d381ff3526304
Olivier MATROT <omatrot@free.fr>
2021-01-28 13:05:29 +0100

    Added HTML file

--- wkwebview/ViewController.swift
+++ wkwebview/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let bundle = Bundle.main
+        let path = bundle.path(forResource: "map", ofType: "html")
+        var html = ""
+        do {
+            try html = String(contentsOfFile: path!, encoding: .utf8)
+        } catch {
+        //ERROR
+        }
+
+        webView.loadHTMLString(html, baseURL: nil)
+        webView.allowsBackForwardNavigationGestures = true
     }
 
-
+    override func loadView() {
+    webView = WKWebView()
+    webView.navigationDelegate = self
+    view = webView
+    }
 }
 
